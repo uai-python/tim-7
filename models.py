6c78cf26c8c097ab2d4f65e53b1f38c7b044339b
ghinarh <ghinarh@gmail.com>
2017-04-05 19:54:08 +0700

    Add files via upload

---
+++ models.py
@@ -0,0 +1,40 @@
+from config import db
+
+class pemain(db.Model):
+    __tablename__ = 'guest'
+
+    id_pemain = db.Column(db.Integer, primary_key=True)
+	salut = db.Column(db.Varchar(15))
+	nama = db.Column(db.Varchar(30))
+    usia = db.Column(db.Integer)
+    jenis_kelamin = db.Column(db.Varchar(10))
+	email = db.Column(db.Varchar(20))
+    Kota/Kabupaten = db.Column(db.Varchar(20))
+
+    def __init__(self, salut, nama, usia, jenis_kelamin, email, Kota/Kabupaten, id_pemain):
+		self.salut= salut
+		self.nama= nama
+        self.usia = usia
+        self.jenis_kelamin = jenis_kelamin
+		self.email= email
+        self.Kota/Kabupaten = Kota/Kabupaten
+		self.id_pemain= id_pemain
+        self.user = user
+
+    def __repr__(self):
+        return '<Guest %r>' % self.user
+
+
+class Question(db.Model):
+    __tablename__ = 'question'
+
+    idquestion = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String)
+    answer = db.Column(db.String(30))
+
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+
+    def __repr__(self):
+        return '<Question %r>' % self.question
